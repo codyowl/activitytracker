74928da120f1af23dfe903514e8f76089a6f570b
codyowl <codyowl@gmail.com>
2017-01-15 21:13:57 +0530

    Heroku changes :heavy_plus_sign:

--- activitytracker/settings.py
+++ activitytracker/settings.py
@@ -87,10 +87,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-        )
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
