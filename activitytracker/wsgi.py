f11d3b9c9d60bbd13e39830147fa6f89f46521c2
codyowl <codyowl@gmail.com>
2017-01-16 09:01:58 +0530

    wsgi tweaked to fix error :memeo:

--- activitytracker/wsgi.py
+++ activitytracker/wsgi.py
@@ -7,15 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+# import os
+# from django.core.wsgi import get_wsgi_application
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "activitytracker.settings")
+# from whitenoise.django import DjangoWhiteNoise
+
+
+
+
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
+
 import os
+
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "activitytracker.settings")
+
 from whitenoise.django import DjangoWhiteNoise
 
-
-
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-
+application = DjangoWhiteNoise(get_wsgi_application())
 
