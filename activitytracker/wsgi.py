f846d5f050f8d8f9d3007032f2191d749eae1bc3
codyowl <codyowl@gmail.com>
2017-01-16 08:41:58 +0530

    wsgi file changed to recover DJANGO_SETTINGS error :memo:

--- activitytracker/wsgi.py
+++ activitytracker/wsgi.py
@@ -9,10 +9,13 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "activitytracker.settings")
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "activitytracker.settings")
+
 
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+
+
